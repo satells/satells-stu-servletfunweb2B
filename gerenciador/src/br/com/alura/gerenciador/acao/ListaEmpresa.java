f0ed459adb2d316c876f097c7579631ddc472d11
mauro <mauro@mauro-PC>
2021-01-26 15:48:52 -0300

    alterações gerais

--- gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresa.java
+++ gerenciador/src/br/com/alura/gerenciador/acao/ListaEmpresa.java
@@ -1,23 +1,19 @@
-package br.com.alura.gerenciador.servlet;
+package br.com.alura.gerenciador.acao;
 
 import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import br.com.alura.gerenciador.modelo.Banco;
 import br.com.alura.gerenciador.modelo.Empresa;
 
-@WebServlet("/listaEmpresassss")
-public class ListaEmpresasServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+public class ListaEmpresa {
 
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void executa(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	Banco banco = new Banco();
 	List<Empresa> lista = banco.getEmpresas();
