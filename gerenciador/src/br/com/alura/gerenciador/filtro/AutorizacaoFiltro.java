651e1c19f7109308e34ac4cbca1ef71ef86ddb98
mauro <mauro@mauro-PC>
2021-02-04 15:45:19 -0300

    alterações gerais

--- gerenciador/src/br/com/alura/gerenciador/filtro/AutorizacaoFiltro.java
+++ gerenciador/src/br/com/alura/gerenciador/filtro/AutorizacaoFiltro.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -17,6 +18,14 @@ import br.com.alura.gerenciador.controler.Redirect;
 
 //@WebFilter(urlPatterns = "/entrada")
 public class AutorizacaoFiltro implements Filter {
+
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
--- gerenciador/src/br/com/alura/gerenciador/filtro/ControllerFilter.java
+++ gerenciador/src/br/com/alura/gerenciador/filtro/ControllerFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,6 +17,13 @@ import br.com.alura.gerenciador.controler.HttpFlow;
 
 //@WebServlet(urlPatterns = "/entrada")
 public class ControllerFilter implements Filter {
+    @Override
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain) throws IOException, ServletException {
