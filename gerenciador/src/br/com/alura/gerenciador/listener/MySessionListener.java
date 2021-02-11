e7fbc8a444ac2b4d902c29634b3eb68dc78704bf
satells <msergiost@hotmail.com>
2021-02-11 15:15:42 -0300

    alteração

--- gerenciador/src/br/com/alura/gerenciador/listener/MySessionListener.java
+++ gerenciador/src/br/com/alura/gerenciador/listener/MySessionListener.java
@@ -9,17 +9,22 @@ import javax.servlet.http.HttpSessionListener;
 public class MySessionListener implements HttpSessionListener {
     @Override
     public void sessionCreated(HttpSessionEvent se) {
+	System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
 	System.out.println("-- HttpSessionListener#sessionCreated invoked --");
 
 	HttpSession session = se.getSession();
 
 	System.out.println("session id: " + session.getId());
 
-	session.setMaxInactiveInterval(120);// in seconds
+	session.setMaxInactiveInterval(30);// in seconds
+	System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
     }
 
     @Override
     public void sessionDestroyed(HttpSessionEvent se) {
+	System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+
 	System.out.println("-- HttpSessionListener#sessionDestroyed invoked --");
+	System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
     }
 }
